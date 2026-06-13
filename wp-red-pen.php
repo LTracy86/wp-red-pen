4bb9ba802cdd4eccd1b5337e9f48480cdc680ceb
Lincoln Tracy <lincolnmtracy@gmail.com>
2026-06-13 02:20:40 -0400

    Auto-checkpoint 2026-06-13 02:20 - before write to wp-red-pen.php

--- wp-red-pen.php
+++ wp-red-pen.php
@@ -970,7 +970,8 @@ function wprp_print_frontend_assets() {
 				'<span class="wprp-prio wprp-prio-' + esc(n.priority || 'normal') + '">' + esc(n.priorityLabel) + '</span>' +
 				'<span>' + esc(n.author) + '</span><span>' + esc(n.date) + '</span>' +
 				(n.assigneeName ? '<span class="wprp-assignee">&rarr; ' + esc(n.assigneeName) + '</span>' : '') +
-				'<button type="button" class="wprp-resolve">' + (n.resolved ? '<?php echo esc_js( __( 'Reopen', 'wp-red-pen' ) ); ?>' : '<?php echo esc_js( __( 'Resolve', 'wp-red-pen' ) ); ?>') + '</button></div>' +
+				'<button type="button" class="wprp-resolve">' + (n.resolved ? '<?php echo esc_js( __( 'Reopen', 'wp-red-pen' ) ); ?>' : '<?php echo esc_js( __( 'Resolve', 'wp-red-pen' ) ); ?>') + '</button>' +
+				'<button type="button" class="wprp-edit"><?php echo esc_js( __( 'Edit', 'wp-red-pen' ) ); ?></button></div>' +
 				'<div class="wprp-body">' + n.body + '</div>' +
 				(n.shot ? '<a class="wprp-shot" href="' + esc(n.shot) + '" target="_blank" rel="noopener"><img src="' + esc(n.shot) + '" alt="screenshot"></a>' : '') +
 				(n.ctx ? '<div class="wprp-ctx">' + esc(n.ctx) + '</div>' : '') +
