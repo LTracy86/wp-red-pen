d9b054af52c8605bec339bd5d68d7e68d1802923
Lincoln Tracy <lincolnmtracy@gmail.com>
2026-06-14 16:13:37 -0400

    Auto-checkpoint 2026-06-14 16:13 - before write to wp-red-pen.php

--- wp-red-pen.php
+++ wp-red-pen.php
@@ -1448,6 +1448,7 @@ function wprp_print_frontend_assets() {
 				return notes;
 			}).catch(function () {
 				list.innerHTML = '<p class="wprp-muted"><?php echo esc_js( __( 'Could not load notes.', 'wp-red-pen' ) ); ?></p>';
+					list.setAttribute('aria-busy', 'false');
 			});
 		}
 
