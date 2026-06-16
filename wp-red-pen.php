9071e26638dec12bd56f7956952001295a099903
Lincoln Tracy <lincolnmtracy@gmail.com>
2026-06-16 11:26:20 -0400

    Auto-checkpoint 2026-06-16 11:26 - before write to wp-red-pen.php

--- wp-red-pen.php
+++ wp-red-pen.php
@@ -1212,6 +1212,9 @@ function wprp_note_to_array( $note, $replies = null ) {
 		'level'       => $level,
 		'ctxKey'      => (string) get_post_meta( $note->ID, WPRP_META_CTXKEY, true ),
 		'ctxLabel'    => (string) get_post_meta( $note->ID, WPRP_META_CTXLABEL, true ),
+		'agent'       => (string) get_post_meta( $note->ID, WPRP_META_AGENT, true ),
+		'agentLabel'  => wprp_agent_label( (string) get_post_meta( $note->ID, WPRP_META_AGENT, true ) ),
+		'codeScope'   => (string) get_post_meta( $note->ID, WPRP_META_CODESCOPE, true ),
 		'replies'     => array_map( 'wprp_reply_to_array', is_array( $replies ) ? $replies : wprp_get_replies( $note->ID ) ),
 	);
 }
