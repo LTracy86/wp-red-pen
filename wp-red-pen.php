4e0cea245fa9b16e6434aa3bb542028be6d07147
Lincoln Tracy <lincolnmtracy@gmail.com>
2026-07-01 13:38:52 -0400

    Auto-checkpoint 2026-07-01 13:38 - before write to uninstall.php

--- wp-red-pen.php
+++ wp-red-pen.php
@@ -2373,8 +2373,8 @@ add_action(
 					'url'         => esc_url_raw( home_url( add_query_arg( array() ) ) ),
 					'page'        => $ctx['page'],     // { key, label, target }
 					'template'    => $ctx['template'], // { key, label }
-					'global'      => $ctx['global'],   // { key, label } - site-wide, every view
-					'priming'     => wprp_priming_data_reviewer( array_values( array_filter( array( $ctx['page']['key'], $ctx['template']['key'], $ctx['global']['key'] ) ) ) ),
+					// No 'global' for reviewers: they never see or file site-wide notes.
+					'priming'     => wprp_priming_data_reviewer( array_values( array_filter( array( $ctx['page']['key'], $ctx['template']['key'] ) ) ) ),
 				)
 			);
 		} else {
